1e8bfbb2e236743bbf65b687580f40d68811737c
Yeison <pepsimango234@gmail.com>
2025-10-08 00:39:29 -0500

    Vista Registrar Nota con estructura visual y navegación

---
+++ src/admin/components/PaginaRegistrarNota/BotonRegistrar.jsx
@@ -0,0 +1,7 @@
+export default function BotonRegistrar() {
+  return (
+    <button type="button" className="btn btn-primary btn-block mb-4">
+      Registrar Nota
+    </button>
+  );
+}
--- src/admin/components/PaginaSeguimientoNotas/EncabezadoNotas.jsx
+++ src/admin/components/PaginaSeguimientoNotas/EncabezadoNotas.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
+
 export default function EncabezadoNotas() {
-    return (
-        <>
-            <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 className="h3 mb-0 text-gray-800">Seguimiento de Notas</h1>
-                <a href="crear-nota.html" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                    <i className="fas fa-plus fa-sm text-white-50"></i> Registrar Nota
-                </a>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800">Seguimiento de Notas</h1>
+        <Link
+          to="/registrar-nota"
+          className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+        >
+          <i className="fas fa-plus fa-sm text-white-50"></i> Registrar Nota
+        </Link>
+      </div>
+    </>
+  );
 }
